46fc78e8fa0b8f1383936ba9c7983f29c6c2ea17
Laize Ferraz <laizeferraz@gmail.com>
2021-05-30 21:23:48 +1000

    updated ex2

--- answers/ex2.js
+++ answers/ex2.js
@@ -1,17 +1,13 @@
-var inputArr = [8, 4, 2, 3, 5];
- 
-let bubbleSort = (inputArr) => {
-   let len = inputArr.length;
-   for (let i = 0; i < len; i++) {
-       for (let j = 0; j < len; j++) {
-           if (inputArr[j] > inputArr[j + 1]) {
-               let tmp = inputArr[j];
-               inputArr[j] = inputArr[j + 1];
-               inputArr[j + 1] = tmp;
-           }
-       }
-   }
-   return inputArr;
-};
- 
-console.log(bubbleSort(inputArr));
+const inputArr1 = [8, 4, 2, 3, 5];
+const inputArr2 = [33, 4, 12, 8, 7, 9];
+let mutualElements = [];
+
+for (let i = 0; i < inputArr1.length; i++) {
+  for (let j = 0; j < inputArr2.length; j++) {
+    if (inputArr1[i] === inputArr2[j]) {
+      mutualElements.push(inputArr1[i]);
+    }
+  }
+}
+
+console.log(mutualElements);
--- ex2.js
+++ ex2.js
@@ -1,22 +1,15 @@
 // the following code has errors, find them and fix them and use node to check it is working
 
-var inputArr = [8, 4, 2, 3, 5];
- 
-let bubbleSort = (inputArray) => {
-   let len = inputArr.length;
-   for (let i = 0; i < len; i++) {
-       for (let j = 0; j < len; i++) {
-           if (inputArr[j] > inputArr[j + 1]) {
-               let tmp = inputArr[j];
-               inputArr[j] = inputArr[j + 1];
-               inputArr[j + 1] = tmp;
-           }
-       }
-   }
-   
-};
- 
-console.log(bubbleSort(inputArr));
+const inputArr1 = [8, 4, 2, 3, 5];
+const inputArr2 = [33, 4, 12, 8, 7, 9];
+let mutualElements = [];
 
-// diagram for bubble sort for context : https://www.productplan.com/uploads/bubble-sort-1024x683-2.png
-// the above function when working should take the array and sort it from lowest to highest
+for (let i = 0; i < len; i++) {
+  for (let j = 0; j < len; i++) {
+    if ((inputArr1[j] = inputArr2[i])) {
+      mutualElements.slice(inputArr1[j]);
+    }
+  }
+}
+
+console.log(mutualElements);
