d42b6c86e3619a5b5fab0c327f1c964efc254aa3
Laize Ferraz <laizeferraz@gmail.com>
2021-06-01 16:23:31 +1000

    ex7.js first challenge solution.

--- answers/ex7.js
+++ answers/ex7.js
@@ -10,16 +10,34 @@ let students = [
 console.log(students.toString());
 
 // log length of the array
-console.log(students.length);
+// console.log(students.length);
 
 // log the last element
-console.log(students[students.length - 1]);
+// console.log(students[students.length - 1]);
 
 // add john to array
 students.push(206, "John", 45);
-console.log(students.toString());
+// console.log(students.toString());
 
 // remove the first item from the array
 let removedStudent = students.shift();
-console.log(students.toString());
-console.log(removedStudent);
+// console.log(students.toString());
+// console.log(removedStudent);
+
+// Challenge - Use a for loop through the array to search for ID = 205 and update Sue’s name to Susan
+for (i = 0; i < students.length; i++) {
+  // as we loop through the students array, we loop through each nested student array item
+  for (j = 0; j < students[i].length; j++) {
+    // we check each item in the nested students array until we find one that matches our id
+    if (students[i][j] === 205) {
+      // we check inside this nested student array item to find the position of the index of Sue
+      let position = students[i].indexOf("Sue");
+      // when we find the index and store it in position, we can now use it to target and change Sue to Susan
+      students[i][position] = "Susan";
+    }
+  }
+}
+
+console.log(students);
+
+// Challenge - Remove an item from the middle of the array. ID=204
--- ex4.js
+++ ex4.js
@@ -1,26 +1,25 @@
 // the following code has errors, find them and fix them and use node to check it is working
 
 const morningGreetings = (name) => {
-    console.log(`Good morning ${name}`); 
+  console.log(`Good morning ${name}`); 
+}
+const eveningGreeting = function (firstName) {
+  console.log(`Good evening ${name}`);
+}
+ 
+const todaysGreeting = () {
+  morningGreetings('Lisa')
+  console.log(`Thanks for all you have done during the day`);
+  eveningGreeting('Lisa”);
+}
+ 
+todaysGreeting();
+ 
+ function myCounter ()  {
+  let count = 0
+  return function () {
+     ++count;
   }
-  const eveningGreeting = function (firstName) {
-    console.log(`Good evening ${name}`);
-  }
-   
-  const todaysGreeting = () {
-    morningGreetings('Lisa')
-    console.log(`Thanks for all you have done during the day`);
-    eveningGreeting('Lisa”);
-  }
-   
-  todaysGreeting();
-   
-   function myCounter ()  {
-    let count = 0
-    return function () {
-       ++count;
-    }
-  }
-  const noOfTimes = myCounters();
-  console.log(noOfTimes(“am I meant to be here?”)); 
-  
+}
+const noOfTimes = myCounters();
+console.log(noOfTimes(“am I meant to be here?”)); 
